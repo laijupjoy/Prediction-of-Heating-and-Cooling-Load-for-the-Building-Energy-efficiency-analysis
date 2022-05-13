7ef20478f407fd6a90c1a4f838475baa5f1d178f
LAIJU P JOY <87544051+laijupjoy@users.noreply.github.com>
2022-05-13 23:01:09 +0530

    Add files via upload

---
+++ static/script/script.js
@@ -0,0 +1,10 @@
+alert('The Page Has Been Loaded')
+function myFunction() {
+    var txt;
+    if (confirm("Press a button!")) {
+      txt = "You pressed OK!";
+    } else {
+      txt = "You pressed Cancel!";
+    }
+    document.getElementById("demo").innerHTML = txt;
+  }
